6651a3df2ee33ae1190e96692595271166934ab6
wenwill <j6wen@ucsd.edu>
2024-11-12 15:32:02 -0800

    add WearOS GUI

---
+++ WearOS-GUI/app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.wearos_gui"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.wearos_gui"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.play.services.wearable)
+    implementation(libs.wear)
+}
