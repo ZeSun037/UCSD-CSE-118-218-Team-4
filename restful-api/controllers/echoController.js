448399d1c8c2cedbf160d0e981571f9918a667b8
ZeSun037 <zesun@ucsd.edu>
2024-12-05 16:11:44 -0800

    Unified format for TODO objects in Redis

--- restful-api/controllers/echoController.js
+++ restful-api/controllers/echoController.js
@@ -1,47 +1,56 @@
 const express = require('express');
+const crypto = require('crypto');
 const Redis = require('../init/initRedis');
 
 exports.postUsersTodos = async (req, res) => {
     const redis = await Redis.getConnection();
 
-    const newTODOs = req.body;
+    const todo = req.body;
+    const { assignee, task, place, time, priority } = todo;
+    console.log("assignee: ",assignee, ", task: ", task, ", place: ", place, ", time: ", time);
 
-    const testTODOs = [
-        {
-            userId: "123",
-            field: "Buy magazine",
-            value: `Title: Buy magazine, Priority: HIGH, Places: STORE, Time: GENERAL, Assignee: , isDone: false`
-        },
-        {
-            userId: "123",
-            field: "Buy battery",
-            value: `Title: Buy battery, Priority: MEDIUM, Places: STORE, Time: GENERAL, Assignee: , isDone: false`
-        }
-    ]
-
-    for (const todo of testTODOs) {
-        const exists = await redis.exists(todo.userId);
-        if (exists === 1) {
-
-            console.log(`${todo.userId} already exists in the database!`);
-            const insert = await redis.hSet(todo.userId, todo.field, todo.value);
-
-            if (insert === 1) {
-                console.log(`Successfully created new TODOs for ${todo.userId}.`);
-            }
-
-        } else {
-
-            console.log(`Creating new TODOs for ${todo.userId}...`);
-            const insert = await redis.hSet(todo.userId, todo.field, todo.value);
-
-            if (insert === 1) {
-                console.log(`Successfully created new TODOs for ${todo.userId}.`);
-            }
-
-        }
-        console.log(await redis.hGetAll(todo.userId));
+    const todoItem = {
+        userId: "123",
+        field: `${task}`,
+        value: `Title: ${task}, Priority: ${priority}, Place: ${place}, Time: ${time}, Assignee: ${assignee}, isDone: false`
     }
+
+    // const testTODOs = [
+    //     {
+    //         userId: "123",
+    //         field: "Buy magazine",
+    //         value: `Title: Buy magazine, Priority: HIGH, Places: STORE, Time: GENERAL, Assignee: , isDone: false`
+    //     },
+    //     {
+    //         userId: "123",
+    //         field: "Buy battery",
+    //         value: `Title: Buy battery, Priority: MEDIUM, Places: STORE, Time: GENERAL, Assignee: , isDone: false`
+    //     }
+    // ]
+
+    todoItem.userId = crypto.createHash('sha256').update(assignee).digest('hex');
+
+    const exists = await redis.exists(todoItem.userId);
+    if (exists === 1) {
+
+        console.log(`${todoItem.userId} already exists in the database!`);
+        const insert = await redis.hSet(todoItem.userId, todoItem.field, todoItem.value);
+
+        if (insert === 1) {
+            console.log(`Successfully created new TODOs for ${todoItem.userId}.`);
+        }
+
+    } else {
+
+        console.log(`Creating new TODOs for ${todoItem.userId}...`);
+        const insert = await redis.hSet(todoItem.userId, todoItem.field, todoItem.value);
+
+        if (insert === 1) {
+            console.log(`Successfully created new TODOs for ${todoItem.userId}.`);
+        }
+
+    }
+    console.log(await redis.hGetAll(todoItem.userId));
     res.status(200).send(`Successfully created new TODOs.`);
 }
 
